c12c3c51b711b1558f96550b3f67829c97f02085
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-12 14:24:43 +0200

    Brew formula update for bubbly version v0.0.1-alpha.1

--- Formula/bubbly.rb
+++ Formula/bubbly.rb
@@ -5,24 +5,24 @@
 class Bubbly < Formula
   desc "Bubbly CLI"
   homepage "https://bubbly.dev/"
-  version "0.0.1-alpha.2"
+  version "0.0.1-alpha.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.2/bubbly_0.0.1-alpha.2_darwin_amd64.tar.gz"
-    sha256 "686e360698e43fd8f6d32f40d41e1e5c4b037dbbe8950362b002575d0f566945"
+    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.1/bubbly_0.0.1-alpha.1_darwin_amd64.tar.gz"
+    sha256 "9ff56ed0463f768d1048f5bea2f77e66d49560a9e4117b1220e0d436dd99dacb"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.2/bubbly_0.0.1-alpha.2_darwin_arm64.tar.gz"
-    sha256 "b1358a1f387b8b6f19822ebfc92dd0e33017081be3960c9eb3fc0785568898de"
+    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.1/bubbly_0.0.1-alpha.1_darwin_arm64.tar.gz"
+    sha256 "3df87aa08559c8c367df54ed8fc1dcf409dfc01ae26b7f50129a4ca973332de1"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.2/bubbly_0.0.1-alpha.2_linux_amd64.tar.gz"
-    sha256 "d8610bf8d66fcaa07b092a465b74c12ccafeddd0ff0975158967ca01a1e514a6"
+    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.1/bubbly_0.0.1-alpha.1_linux_amd64.tar.gz"
+    sha256 "3706464dd161f58f9fe78ef2ba41e25e6ef8af1b039e1d46cf4acc43a29cb7dc"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.2/bubbly_0.0.1-alpha.2_linux_arm64.tar.gz"
-    sha256 "00d5774943e10271f97cb94d9e36b051141d9097822d62a7fc7e495023885550"
+    url "https://github.com/verifa/bubbly/releases/download/v0.0.1-alpha.1/bubbly_0.0.1-alpha.1_linux_arm64.tar.gz"
+    sha256 "553a15488303719c958a62fce2826fbfb2c85a9c5278d66c6a1b74b283020270"
   end
 
   depends_on "go"
